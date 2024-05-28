88f831b726d9ce805a934b7a758ad95dad702c61
Franco Canzani <francocanzani@gmail.com>
2024-05-28 14:22:37 +0200

    adds filter president by year

--- app/api/endpoints/president.py
+++ app/api/endpoints/president.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.utils.json_parser import parse_json
+from datetime import datetime
 
 router = APIRouter(
     prefix="/api/v1/President",
@@ -33,6 +34,27 @@ def get_president_by_name(president_name: str):
         if str(president_name).lower() in president["name"].lower()
         or str(president_name).lower() in president["lastName"].lower()
     ]
+
+    if not filtered_presidents:
+        raise HTTPException(status_code=404, detail="President not found")
+
+    return filtered_presidents
+
+@router.get("/year/{period_year}")
+def get_president_by_year(period_year: str):
+    filtered_presidents = []
+
+    for president in presidentes_data:
+        start_year = int(president["startPeriodDate"][:4])
+        end_year = None
+
+        if president["endPeriodDate"].lower() == "presente":
+            end_year = datetime.now().year
+        else:
+            end_year = int(president["endPeriodDate"][:4])
+
+        if (int(period_year) >= start_year) and (int(period_year) <= end_year):
+            filtered_presidents.append(president)
 
     if not filtered_presidents:
         raise HTTPException(status_code=404, detail="President not found")
